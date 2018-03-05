cf74c6e670a45edbc4b915c28669d3018711cbe0
Carlos Eduardo Garcia Tejeda <carlos.garcia@ctplc.com>
2018-03-05 17:56:43 -0600

    Cargando estilos desde webpack

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname,'..', 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'..', 'dist')
+    },
+    module:{
+        rules:[
+            //aquí van los loaders
+            {
+                test: /\.css$/,//que tipo de archivo quiero reconocer, se usar regex
+                use:["style-loader","css-loader"] //que loader se va a encargar del archivo (tiene que ir en ese orden)
+            }
+        ]
+    }
+}
+
+/*
+    Loaders: 
+    style-loader: imprime los estilos en html
+    css-loader: permirte interpretar codigo css entro de javascript,
+*/
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+import './estilos.css'
+document.write("hola mundo");
 console.log("hola mundo desde webpack!");
