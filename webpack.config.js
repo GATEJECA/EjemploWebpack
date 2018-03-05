ca9ec121ff9998c0b88feb47d5869f510a600390
Carlos Eduardo Garcia Tejeda <carlos.garcia@ctplc.com>
2018-03-05 17:07:47 -0600

    Hola mundo con webapack.config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+/*
+El entry point no requeriría de path para componer la ruta, ya que la ruta es relativa al proyecto.
+Donde si requires path es para el path the output.
+https://stackoverflow.com/questions/35048686/difference-between-path-resolve-and-path-join-invocation
+*/
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
