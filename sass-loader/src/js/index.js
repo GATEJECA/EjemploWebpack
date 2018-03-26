0545c80cc40e970d50224fdb60fdbf1b62322f2f
Carlos Garcia <carlos.garcia@ctplc.com>
2018-03-26 12:41:26 -0600

    webpack sass

---
+++ sass-loader/src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/estilos.css';
+import imagen from '../images/descarga.jpg';
+import { mimensaje as peluches, delayeMessage as mensajeconretardo } from './mensaje';
+import data from "./teachers.json";
+import React from 'react';
+import {render} from 'react-dom';
+import Teachers from "./componentes/teachers";
+
+render(<Teachers data={data} />, document.getElementById("Container"));
